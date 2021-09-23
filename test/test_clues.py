d570eabb697cb91a8d39194006c5b4d1d598fb2d
micafer <micafer1@upv.es>
2021-09-23 16:35:09 +0200

    Minor change

--- test/test_clues.py
+++ test/test_clues.py
@@ -1,3 +1,20 @@
+#!/usr/bin/env python
+#
+# CLUES - Cluster Energy Saving System
+# Copyright (C) 2018 - GRyCAP - Universitat Politecnica de Valencia
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from mock import MagicMock
 import unittest
 import sys
@@ -20,7 +37,8 @@ class TestCLUES(unittest.TestCase):
         tests_path = os.path.dirname(os.path.abspath(__file__))
         sys.argv = ["test_clues.py", "-s"]
         options = MagicMock()
-        options.SIM_FILE = os.path.join(tests_path, '../etc/simulator/simple.sim')
+        options.SIM_FILE = os.path.join(tests_path,
+                                        '../etc/simulator/simple.sim')
         options.OUT_FILE = "./cluessim.db"
         options.TRUNCATE = True
         options.RT_MODE = False
